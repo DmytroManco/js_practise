32e0ec9e32107ebd4de3b71f36034a2af6168370
Dmytro Putrin <moncodp@gmail.com>
2019-01-03 10:39:53 +0200

    [Algorithms] some solitions

--- algorithms/common.js
+++ algorithms/common.js
@@ -48,7 +48,7 @@ function maxCharacter(str) {
     const characterArr = str.replace(/\s/g, '').split('');
 
     const charactersObj = characterArr.reduce((curr, next) => {
-        if(!curr.hasOwnProperty(next) && next) {
+        if (!curr.hasOwnProperty(next)) {
             curr[next] = 1;
             return curr;
         }
@@ -63,3 +63,101 @@ function maxCharacter(str) {
 
     return sortedCharacterArr[0][0];
 }
+
+// Anagrams are words or phrases that contain the same number of characters. Create a function that checks for this.
+function characterCounter(str) {
+    return str
+        .replace(/\s/g)
+        .split('')
+        .reduce((obj, character) => {
+            if (!obj.hasOwnProperty(character)) {
+                obj[character] = 1;
+                return obj;
+            }
+
+            if (obj.hasOwnProperty(character)) {
+                obj[character] = obj[character] + 1;
+                return obj;
+            }
+        }, {})
+}
+
+function checkAnagram(str1, str2) {
+    const characterCounter1 = characterCounter(str1);
+    const characterCounter2 = characterCounter(str2);
+
+    if (Object.keys(characterCounter1).length !== Object.keys(characterCounter2).length) return false;
+
+    for (const char in characterCounter1) {
+        if (characterCounter1[char] !== characterCounter2[char]) return false;
+    }
+
+    return true;
+}
+
+function checkAnagram2(str1, str2) {
+    const sortedStr1 = str1.replace(/\s/g, '').split('').sort().join();
+    const sortedStr2 = str2.replace(/\s/g, '').split('').sort().join();
+
+    return sortedStr1 === sortedStr2;
+}
+
+// Given a string of words or phrases, count the number of vowels
+function countVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let counter = 0;
+
+    for (let character of str.toLowerCase()) {
+        if (vowels.includes(character)) {
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+function countVowels2(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+// Array Chunking. Given an array and a size, split the array items into a list of arrays of the given size
+function chunkArr(arr, chunks) {
+    const newArr = [];
+
+    while (arr.length) {
+        const chunk = arr.splice(0, chunks);
+        newArr.push(chunk);
+    }
+
+    return newArr;
+}
+
+//  Reverse Array. Given an array of items, reverse the order.
+function reverseArr(arr) {
+    const reversed = [];
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        reversed.push(arr[i]);
+    }
+
+    return reversed;
+}
+
+// Given a phrase, reverse the order of the characters of each word.
+function reverseWord(str) {
+    const stringArr = str.split(' ').map(reversed);
+
+    return stringArr.join(' ');
+}
+
+//  Capitalization. Given a phrase, capitalize every word.
+function capitalization(str) {
+    return str
+        .split(' ')
+        .map(word => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+        .join(' ');
+}
+
+// Given a phrase, substitute each character by shifting it up or down the alphabet by a given integer.
+// If necessary, the shifting should wrap around back to the beginning or end of the alphabet.
